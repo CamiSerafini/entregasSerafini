f1489cb1497cad995992bc7cb6e81d9497e52cdd
Camila Rocio Serafini <camiiserafinii@gmail.com>
2025-03-18 22:24:09 -0300

    Simbolos de + y - funcionando bastante bien, chequear si realmente bien

--- carrito.js
+++ carrito.js
@@ -1,10 +1,10 @@
 //PARA LA PÁGINA DEL CARRITO
 const conteiner = document.getElementById("ventas");
 
-const elementos = JSON.parse(localStorage.getItem("Carrito"));
-
 /** Crea las tarjetas de productos teniendo en cuenta la lista en bicicletas.js */
 function prodCarrito(){
+    conteiner.innerHTML = ``;
+    const elementos = JSON.parse(localStorage.getItem("Carrito"));
     if(elementos && elementos.length>0){
         elementos.forEach(element => {
             const accesorioCarrito = document.createElement("div");
@@ -15,45 +15,73 @@ function prodCarrito(){
             <p class="precio">$${element.precio}</p>
             <div class="suma-resta">
                 <button>-</button>
-                <span class="cantidad">0</span>
+                <span class="cantidad">${element.cantidad}</span>
                 <button>+</button>
             </div>
             `
             conteiner.appendChild(accesorioCarrito);
-            accesorioCarrito.getElementsByTagName("button")[1].addEventListener("click",() => agregarAlCarrito(element));
-            accesorioCarrito.getElementsByTagName("button")[0].addEventListener("click",() => restarAlCarrito(element));
+            accesorioCarrito.getElementsByTagName("button")[1].addEventListener("click",(e) => {
+                // agregarAlCarrito(element);
+                const cuenta = e.target.parentElement.getElementsByTagName("span")[0];
+                cuenta.innerText = agregarAlCarrito(element);
+            });
+            accesorioCarrito.getElementsByTagName("button")[0].addEventListener("click",() => {
+                restarAlCarrito(element);
+                prodCarrito();
+            }); 
         });
 
     };
 }
+
 prodCarrito();
 
 function agregarAlCarrito(element){
+    let elementos = JSON.parse(localStorage.getItem("Carrito"));
+    let cuenta = 0;
     if(!elementos || elementos.length === 0){ //En caso de que aun no haya nada en el carrito
         const nuevoProducto = element;
         nuevoProducto.cantidad = 1;
         localStorage.setItem("Carrito",JSON.stringify([nuevoProducto]));
+        cuenta = 1;
     }else{ //Hay algo pero tengo que ver si del producto que agrego es el primero o no 
         const ind = elementos.findIndex(car=> car.nom === element.nom);
         const nuevoCarrito = elementos;
         if(ind === -1){ //Primero que agrego de ese producto
             element.cantidad = 1;
             nuevoCarrito.push(element);
+            cuenta = 1;
         }else{ //Ya se habia pedido algo de ese producto
             nuevoCarrito[ind].cantidad++;
+            cuenta = nuevoCarrito[ind].cantidad;
         }
         localStorage.setItem("Carrito",JSON.stringify(nuevoCarrito));
         actualizarNumero();
+        return cuenta;
     }
 } 
+
+function restarAlCarrito(element){
+    let elementos = JSON.parse(localStorage.getItem("Carrito"));
+    const ind = elementos.findIndex(car=> car.nom === element.nom);
+    if(elementos[ind].cantidad ===1){
+        //borro todo el elemento
+        elementos.splice(ind,1);
+    }else{
+        elementos[ind].cantidad--;
+    }
+    localStorage.setItem("Carrito",JSON.stringify(elementos));
+}
 
 const cuentaCarrito = document.getElementById("cuenta-carrito");
 
 function actualizarNumero(){
+    let elementos = JSON.parse(localStorage.getItem("Carrito"));
     const suma = elementos.reduce((acum,current)=>acum + current.cantidad, 0);
     cuentaCarrito.innerText = suma;
 }
 
 actualizarNumero();
+
 
 
