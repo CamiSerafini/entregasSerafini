1c8eb648f20f9af73b385ec15f447f4788616cdb
Camila Rocio Serafini <camiiserafinii@gmail.com>
2025-03-19 16:50:51 -0300

    ultimando detalles

--- js/carrito.js
+++ js/carrito.js
@@ -4,11 +4,11 @@ let unidadesElement = document.getElementById("unidades");
 let precioElement = document.getElementById("precio-total");
 let reinicia = document.getElementById("reiniciar");
 
-/** Crea las tarjetas de productos teniendo en cuenta la lista en bicicletas.js */
-function prodCarrito(){
+/** Crea las tarjetas de productos teniendo en cuenta la lista en carrito.json */
+function prodCarrito() {
     conteiner.innerHTML = ``;
     const elementos = JSON.parse(localStorage.getItem("Carrito"));
-    if(elementos && elementos.length>0){
+    if (elementos && elementos.length > 0) {
         elementos.forEach(element => {
             const accesorioCarrito = document.createElement("div");
             accesorioCarrito.classList = "tarjeta-producto-carrito"
@@ -23,20 +23,18 @@ function prodCarrito(){
             </div>
             `
             conteiner.appendChild(accesorioCarrito);
-            accesorioCarrito.getElementsByTagName("button")[1].addEventListener("click",(e) => {
-                // agregarAlCarrito(element);
+            accesorioCarrito.getElementsByTagName("button")[1].addEventListener("click", (e) => {
                 const cuenta = e.target.parentElement.getElementsByTagName("span")[0];
                 cuenta.innerText = agregarAlCarrito(element);
                 actualizarTotales();
             });
-            accesorioCarrito.getElementsByTagName("button")[0].addEventListener("click",() => {
+            accesorioCarrito.getElementsByTagName("button")[0].addEventListener("click", () => {
                 restarAlCarrito(element);
                 prodCarrito();
                 actualizarTotales();
-            }); 
+            });
         });
-
-    }else{
+    } else {
         localStorage.removeItem("Carrito");
     }
 }
@@ -44,85 +42,85 @@ function prodCarrito(){
 prodCarrito();
 actualizarTotales();
 
-function agregarAlCarrito(element){
+function agregarAlCarrito(element) {
     let elementos = JSON.parse(localStorage.getItem("Carrito"));
     let cuenta = 0;
-    if(!elementos || elementos.length === 0){ //En caso de que aun no haya nada en el carrito
+    if (!elementos || elementos.length === 0) { //En caso de que aun no haya nada en el carrito
         const nuevoProducto = element;
         nuevoProducto.cantidad = 1;
-        localStorage.setItem("Carrito",JSON.stringify([nuevoProducto]));
+        localStorage.setItem("Carrito", JSON.stringify([nuevoProducto]));
         cuenta = 1;
-    }else{ //Hay algo pero tengo que ver si del producto que agrego es el primero o no 
-        const ind = elementos.findIndex(car=> car.nom === element.nom);
+    } else { //Hay algo pero tengo que ver si del producto que agrego es el primero o no 
+        const ind = elementos.findIndex(car => car.nom === element.nom);
         const nuevoCarrito = elementos;
-        if(ind === -1){ //Primero que agrego de ese producto
+        if (ind === -1) { //Primero que agrego de ese producto
             element.cantidad = 1;
             nuevoCarrito.push(element);
             cuenta = 1;
-        }else{ //Ya se habia pedido algo de ese producto
+        } else { //Ya se habia pedido algo de ese producto
             nuevoCarrito[ind].cantidad++;
             cuenta = nuevoCarrito[ind].cantidad;
         }
-        localStorage.setItem("Carrito",JSON.stringify(nuevoCarrito));
+        localStorage.setItem("Carrito", JSON.stringify(nuevoCarrito));
         actualizarNumero();
         return cuenta;
     }
-} 
+}
 
-function restarAlCarrito(element){
+function restarAlCarrito(element) {
     let elementos = JSON.parse(localStorage.getItem("Carrito"));
-    if(elementos && elementos.length>0){
-        const ind = elementos.findIndex(car=> car.nom === element.nom);
-        if(elementos[ind].cantidad ===1){
+    if (elementos && elementos.length > 0) {
+        const ind = elementos.findIndex(car => car.nom === element.nom);
+        if (elementos[ind].cantidad === 1) {
             //borro todo el elemento
-            elementos.splice(ind,1);
-        }else{
+            elementos.splice(ind, 1);
+        } else {
             elementos[ind].cantidad--;
         }
-        localStorage.setItem("Carrito",JSON.stringify(elementos));
+        localStorage.setItem("Carrito", JSON.stringify(elementos));
         actualizarNumero();
-    }else{
+    } else {
         localStorage.removeItem("Carrito");
     }
-    
+
 }
 
 const cuentaCarrito = document.getElementById("cuenta-carrito");
 
-function actualizarNumero(){
+function actualizarNumero() {
     let elementos = JSON.parse(localStorage.getItem("Carrito"));
-    if(elementos && elementos.length>0){
-        const suma = elementos.reduce((acum,current)=>acum + current.cantidad, 0);
+    if (elementos && elementos.length > 0) {
+        const suma = elementos.reduce((acum, current) => acum + current.cantidad, 0);
         cuentaCarrito.innerText = suma;
-    }else{
+    } else {
         cuentaCarrito.innerText = 0;
     }
-    
+
 }
 
 actualizarNumero();
 
 
-function actualizarTotales(){
+function actualizarTotales() {
     const elementos = JSON.parse(localStorage.getItem("Carrito"));
     let acumulaUnidad = 0;
     let acumulaPrecio = 0;
-    if(elementos && elementos.length>0){
+    if (elementos && elementos.length > 0) {
         elementos.forEach(element => {
             acumulaUnidad += element.cantidad;
-            acumulaPrecio += element.cantidad*element.precio;
+            acumulaPrecio += element.cantidad * element.precio;
         })
         unidadesElement.innerText = acumulaUnidad;
         precioElement.innerText = acumulaPrecio;
-    }else{
+    } else {
         unidadesElement.innerText = 0;
         precioElement.innerText = 0;
     }
 }
 
-//Necesitaría sacarlo ademas del LocalStorage
-reinicia.addEventListener("click",reiniciarCarrito);
-function reiniciarCarrito(){
+//Cuando reinicio el carrito ecesitaría sacar la clave del LocalStorage
+reinicia.addEventListener("click", reiniciarCarrito);
+function reiniciarCarrito() {
     localStorage.removeItem("Carrito");
     actualizarTotales();
     prodCarrito();
--- js/script.js
+++ js/script.js
@@ -12,8 +12,8 @@ async function getData(url, clave) {
     }
 }
 
-//Solo me va a cargar los datos si el localStorage con esa clave esta vacio, lo hice porque sino siempre que recargo
-//la página se me reiniciaban estos 3 arrays.
+/*Solo me va a cargar los datos si el localStorage con esa clave esta vacio, lo hice porque sino siempre que recargo
+la página se me reiniciaban estos 3 arrays.*/
 getData('/json/adolescents.json', "Adolescentes");
 getData('/json/adultos.json', "Adultos");
 getData('/json/mayores.json', "Mayores");
@@ -21,7 +21,6 @@ getData('/json/mayores.json', "Mayores");
 let adolescentes = JSON.parse(localStorage.getItem("Adolescentes")) || [];
 let adultos = JSON.parse(localStorage.getItem("Adultos")) || [];
 let mayores = JSON.parse(localStorage.getItem("Mayores")) || [];
-
 
 let datosGuardados = [];
 
@@ -50,22 +49,22 @@ function disponibilidad(cupos) {
 }
 
 function mensajeInscripcion(num) {
-    if(!num){
+    if (!num) {
         Swal.fire({
             icon: "warning",
             title: "Lo sentimos, no se ha podido inscribir por falta de cupos",
             showConfirmButton: false,
             timer: 3000
         });
-    }else{
-        if(num==1){
+    } else {
+        if (num == 1) {
             Swal.fire({
                 icon: "success",
                 title: "Inscripción exitosa",
                 showConfirmButton: false,
                 timer: 2000
             });
-        }else{
+        } else {
             Swal.fire({
                 icon: "error",
                 title: "Lo sentimos, no disponemos de clases para esa edad",
@@ -90,7 +89,7 @@ function sacarTurno() {
     edad = parseInt(edad);
     let no = document.getElementById("inscripcionCorrecta");
     let nuevoIngreso = new ingresante(edad);
-    //Los datos se guardaran en el local storage en cualquier caso para tener un registro de todos los contactos
+    //Los datos se guardaran en el local storage en cualquier caso para tener un registro de todos los contactos dentro de datosGuardados
     datosGuardados = JSON.parse(localStorage.getItem("Datos"));
     datosGuardados.push(nuevoIngreso);
     localStorage.setItem("Datos", JSON.stringify(datosGuardados));
@@ -192,14 +191,14 @@ function noRecibir(nombre, apellido) {
     });
 }
 
-function cartelEliminaBien(){
+function cartelEliminaBien() {
     Swal.fire({
         title: "Usted se ha dado de baja correctamente",
         icon: "success"
     });
 }
 
-function cartelNoExiste(){
+function cartelNoExiste() {
     Swal.fire({
         title: "Usted no se encuentra inscripto",
         icon: "error"
@@ -207,7 +206,7 @@ function cartelNoExiste(){
 }
 
 function baja() {
-    cambios=1;
+    cambios = 1;
     let nombre = (document.getElementById("nombreBaja").value).toLowerCase();
     let apellido = (document.getElementById("apellidoBaja").value).toLowerCase();
     let edad = parseInt(document.getElementById("edadBaja").value);
@@ -264,7 +263,6 @@ function baja() {
     if (posicion >= 0) {
         noRecibir(nombre, apellido);
     }
-
 }
 
 //-----------------------------------------------------------------------------------------------------------------------------------------------------
@@ -272,52 +270,52 @@ function baja() {
 getData('/json/productos.json', "Productos");
 let productos = JSON.parse(localStorage.getItem("Productos")) || [];
 
-getData('/json/carrito.json',"Carrito");
+getData('/json/carrito.json', "Carrito");
 let carrito = JSON.parse(localStorage.getItem("Carrito")) || [];
 
 const contenedor = document.getElementById("ventas");
 
-/** Crea las tarjetas de productos teniendo en cuenta la lista en bicicletas.js */
-function crearProductos(productos){
-  productos.forEach(producto => {
-    const nuevoAccesorio = document.createElement("div");
-    nuevoAccesorio.classList = "tarjeta-producto"
-    nuevoAccesorio.innerHTML = `
+/** Crea las tarjetas de productos teniendo en cuenta la lista en productos.json */
+function crearProductos(productos) {
+    productos.forEach(producto => {
+        const nuevoAccesorio = document.createElement("div");
+        nuevoAccesorio.classList = "tarjeta-producto"
+        nuevoAccesorio.innerHTML = `
     <img src="/images/${producto.nom}.jpg" alt="Accesorio patin artistico">
     <h3>${producto.nom}</h3>
     <p class="precio">$${producto.precio}</p>
     <button class="boton-carrito">Agregar al carrito</button>`
-    contenedor.appendChild(nuevoAccesorio);
-    nuevoAccesorio.getElementsByTagName("button")[0].addEventListener("click",() => agregarAlCarrito(producto))
-  });
+        contenedor.appendChild(nuevoAccesorio);
+        nuevoAccesorio.getElementsByTagName("button")[0].addEventListener("click", () => agregarAlCarrito(producto))
+    });
 }
 crearProductos(productos);
 
-function agregarAlCarrito(producto){
+function agregarAlCarrito(producto) {
     let carrito = JSON.parse(localStorage.getItem("Carrito")) || [];
-    if(!carrito || carrito.length === 0){ //En caso de que aun no haya nada en el carrito
+    if (!carrito || carrito.length === 0) { //En caso de que aun no haya nada en el carrito
         const nuevoProducto = producto;
         nuevoProducto.cantidad = 1;
-        localStorage.setItem("Carrito",JSON.stringify([nuevoProducto]));
-    }else{ //Hay algo pero tengo que ver si del producto que agrego es el primero o no 
-        const ind = carrito.findIndex(car=> car.nom === producto.nom);
+        localStorage.setItem("Carrito", JSON.stringify([nuevoProducto]));
+    } else { //Hay algo pero tengo que ver si del producto que agrego es el primero o no 
+        const ind = carrito.findIndex(car => car.nom === producto.nom);
         const nuevoCarrito = carrito;
-        if(ind === -1){ //Primero que agrego de ese producto
+        if (ind === -1) { //Primero que agrego de ese producto
             producto.cantidad = 1;
             nuevoCarrito.push(producto);
-        }else{ //Ya se habia pedido algo de ese producto
+        } else { //Ya se habia pedido algo de ese producto
             nuevoCarrito[ind].cantidad++;
         }
-        localStorage.setItem("Carrito",JSON.stringify(nuevoCarrito));
+        localStorage.setItem("Carrito", JSON.stringify(nuevoCarrito));
         actualizarNumero();
     }
-} 
+}
 
 const cuentaCarrito = document.getElementById("cuenta-carrito");
 
-function actualizarNumero(){
+function actualizarNumero() {
     let carrito = JSON.parse(localStorage.getItem("Carrito")) || [];
-    const suma = carrito.reduce((acum,current)=>acum + current.cantidad, 0);
+    const suma = carrito.reduce((acum, current) => acum + current.cantidad, 0);
     cuentaCarrito.innerText = suma;
 }
 
@@ -329,7 +327,7 @@ actualizarNumero(); // Asi cuando refresco la pagina sigue apareciendo el numero
 //Inicializar "Base de Datos"
 cargaAlumnos();
 
-let cambios=0;
+let cambios = 0;
 
 //SACAR TURNO - 1ER FUNCION
 
@@ -367,12 +365,12 @@ document.getElementById("formuBaja").addEventListener("submit", function (event)
         event.preventDefault();
         const swalWithBootstrapButtons = Swal.mixin({
             customClass: {
-              confirmButton: "btn btn-success",
-              cancelButton: "btn btn-danger"
+                confirmButton: "btn btn-success",
+                cancelButton: "btn btn-danger"
             },
             buttonsStyling: false
-          });
-          swalWithBootstrapButtons.fire({
+        });
+        swalWithBootstrapButtons.fire({
             title: "¿Estas seguro?",
             text: "No podrás revertir esto",
             icon: "warning",
@@ -380,24 +378,26 @@ document.getElementById("formuBaja").addEventListener("submit", function (event)
             confirmButtonText: "Si, estoy seguro",
             cancelButtonText: "No, cancelar por favor!",
             reverseButtons: true
-          }).then((result) => {
+        }).then((result) => {
             if (result.isConfirmed) {
                 baja();
-              swalWithBootstrapButtons.fire({
-                title: "Eliminado",
-                text: "Esperamos verte nuevamente",
-                icon: "success"
-              });
+                swalWithBootstrapButtons.fire({
+                    title: "Eliminado",
+                    text: "Esperamos verte nuevamente",
+                    icon: "success"
+                });
             } else if (
-              result.dismiss === Swal.DismissReason.cancel
+                result.dismiss === Swal.DismissReason.cancel
             ) {
-              swalWithBootstrapButtons.fire({
-                title: "Cancelado",
-                text: "Nos alegra seguir teniendote en nuestras clases",
-                icon: "error"
-              });
+                swalWithBootstrapButtons.fire({
+                    title: "Cancelado",
+                    text: "Nos alegra seguir teniendote en nuestras clases",
+                    icon: "error"
+                });
             }
-          });
+        });
 
     }
 });
+
+// 4TA FUNCION - CARRITO DE COMPRAS!
